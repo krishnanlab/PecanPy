db741ac48d5b3ca70a15421d5bcc091fb852a92a
Remy <liurenmi@msu.edu>
2021-10-30 12:03:17 -0400

    Use formatter class to print default settings

--- src/pecanpy/cli.py
+++ src/pecanpy/cli.py
@@ -25,64 +25,65 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description="Run pecanpy, a parallelized, efficient, and accelerated "
         "Python implementataion of node2vec",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     parser.add_argument(
         "--input",
-        default="graph/karate.edgelist",
-        help="Input graph path",
+        default="graph/karate.edg",
+        help="Input graph (.edg or .npz) file path.",
     )
 
     parser.add_argument(
-        "--output", default="emb/karate.emb", help="Embeddings path",
+        "--output",
+        default="emb/karate.emb",
+        help="Output embeddings (.emd) file path.",
     )
 
     parser.add_argument(
         "--task",
-        nargs="?",
         default="pecanpy",
-        help="Choose task: (pecanpy, todense). Default is pecanpy",
+        help="Task to be performed.",
     )
 
     parser.add_argument(
         "--mode",
-        nargs="?",
         default="SparseOTF",
-        help="Choose mode: (PreComp, SparseOTF, DenseOTF). Default is SparseOTF",
+        help="PecanPy execution mode.",
     )
 
     parser.add_argument(
         "--dimensions",
         type=int,
         default=128,
-        help="Number of dimensions. Default is 128.",
+        help="Number of dimensions.",
     )
 
     parser.add_argument(
         "--walk-length",
         type=int,
         default=80,
-        help="Length of walk per source. Default is 80.",
+        help="Length of walk per source.",
     )
 
     parser.add_argument(
         "--num-walks",
         type=int,
         default=10,
-        help="Number of walks per source. Default is 10.",
+        help="Number of walks per source.",
     )
 
     parser.add_argument(
         "--window-size",
         type=int,
         default=10,
-        help="Context size for optimization. Default is 10. Support list of values",
+        help="Context size for optimization.",
     )
 
     parser.add_argument(
         "--epochs",
-        default=1,
         type=int,
+        default=1,
         help="Number of epochs in SGD when training Word2Vec",
     )
 
@@ -90,34 +91,34 @@ def parse_args():
         "--workers",
         type=int,
         default=0,
-        help="Number of parallel workers. Default is 8. Set to 0 to use all.",
+        help="Number of parallel workers (0 to use all available threads).",
     )
 
     parser.add_argument(
-        "--p", type=float, default=1, help="Return hyperparameter. Default is 1.",
+        "--p",
+        type=float,
+        default=1,
+        help="Return hyperparameter.",
     )
 
     parser.add_argument(
-        "--q", type=float, default=1, help="Inout hyperparameter. Default is 1.",
+        "--q",
+        type=float,
+        default=1,
+        help="Inout hyperparameter.",
     )
 
     parser.add_argument(
         "--weighted",
-        dest="weighted",
         action="store_true",
-        help="Boolean specifying (un)weighted. Default is unweighted.",
+        help="Boolean specifying (un)weighted.",
     )
-    parser.add_argument("--unweighted", dest="unweighted", action="store_false")
-    parser.set_defaults(weighted=False)
 
     parser.add_argument(
         "--directed",
-        dest="directed",
         action="store_true",
-        help="Graph is (un)directed. Default is undirected.",
+        help="Graph is (un)directed.",
     )
-    parser.add_argument("--undirected", dest="undirected", action="store_false")
-    parser.set_defaults(directed=False)
 
     parser.add_argument(
         "--verbose",
@@ -128,7 +129,10 @@ def parse_args():
     parser.set_defaults(verbose=False)
 
     parser.add_argument(
-        "--extend", dest="extend", action="store_true", help="Use node2vec+ extension",
+        "--extend",
+        dest="extend",
+        action="store_true",
+        help="Use node2vec+ extension",
     )
     parser.set_defaults(extend=False)
 
