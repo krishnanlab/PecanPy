0b114b50e2f6f4eaaa28afc4715e01f8844bb0c2
Remy <liurenmi@msu.edu>
2021-11-30 09:48:51 -0500

    Refactor SparseGraph. Extract out functionality of AdjlstGraph that is used for reading edge lists and converting to SparseGraph and DenseGraph.

--- src/pecanpy/graph.py
+++ src/pecanpy/graph.py
@@ -15,52 +15,141 @@ class IDHandle:
         """Update ID list and mapping.
 
         Set IDlst given the input ids and also set the IDmap based on it.
+
         """
         self.IDlst = ids
         self.IDmap = {j: i for i, j in enumerate(ids)}
 
 
-class SparseGraph(IDHandle):
-    """Sparse Graph object that stores graph as adjacency list.
+class AdjlstGraph(IDHandle):
+    """Adjacency list Graph object used for reading/writing edge list files.
+
+    Sparse Graph object that stores graph as adjacency list.
 
     Note:
-        By default the ``SparseGraph`` object converts the data to Compact
-        Sparse Row (csr) format after reading data from an edge list file
-        (``.edg``). This format enables more cache optimized computation.
+        AdjlstGraph is only used for reading/writing edge list files and do not
+        support random walk computations since Numba njit do not work with
+        Python data structures like list and dict.
 
     Examples:
         Read ``.edg`` file and create ``SparseGraph`` object using ``.read_edg``
         method.
 
-        >>> from pecanpy.graph import SparseGraph
+        >>> from pecanpy.graph import AdjlstGraph
         >>>
         >>> # initialize SparseGraph object
-        >>> g = SparseGraph()
+        >>> g = AdjlstGraph()
         >>>
         >>> # read graph from edgelist
-        >>> g.read_edg(path_to_edg_file, weighted=True, directed=False)
+        >>> g.read(path_to_edg_file, weighted=True, directed=False)
         >>>
-        >>> dense_mat = g.to_dense() # convert to dense adjacency matrix
+        >>> indptr, indices, data = g.to_csr()  # convert to csr
         >>>
-        >>> # save the csr graph as npz file to be used later
-        >>> g.save(npz_outpath)
+        >>> dense_mat = g.to_dense()  # convert to dense adjacency matrix
 
     """
 
     def __init__(self):
-        """Initialize SparseGraph object."""
-        super(SparseGraph, self).__init__()
-        self.data = []
-        self.indptr = None
-        self.indices = None
+        """Initialize AdjlstGraph object."""
+        super(AdjlstGraph, self).__init__()
+        self._data = []
 
-    def read_edg(self, edg_fp, weighted, directed, csr=True):
+    @property
+    def num_nodes(self):
+        """Return the number of nodes in the graph."""
+        return len(self._data)
+
+    @staticmethod
+    def _read_edge_line(edge_line, weighted):
+        """Read a line from the edge list tsv file."""
+        # TODO: add option to choose delimiter
+        terms = edge_line.strip().split("\t")
+        id1, id2 = terms[0].strip(), terms[1].strip()
+
+        weight = 1.0 
+        if weighted:
+            if len(terms) != 3:
+                raise ValueError(
+                    f"Expecting three columns in the edge list file for a "
+                    f"weighted graph, got {len(terms)} instead: {edge_line!r}"
+                )
+            weight = float(terms[-1])
+
+        return id1, id2, weight
+
+    @staticmethod
+    def _is_valid_edge_weight(weight):
+        """Check if the edge weight is non-negative."""
+        if weight <= 0:
+            edg_str = f"w({id1},{id2}) = {weight}"
+            print(f"WARNING: non-positive edge ignored: {edg_str}")
+            return False
+        return True
+
+    def _check_edge_existence(self, id1, id2, idx1, idx2, weight):
+        """Check if an edge exists.
+
+        If the edge to be added already exists and the new edge weight is
+        different from the existing edge weights, print warning message.
+
+        """
+        if idx2 in self._data[idx1]:
+            if self._data[idx1][idx2] != weight:
+                print(
+                    f"WARNING: edge from {id1} to {id2} exists, with "
+                    f"value of {self._data[idx1][idx2]:.2f}. "
+                    f"Now overwrite to {weight:.2f}.",
+                )
+
+    def get_node_idx(self, node_id):
+        """Get index of the node and create new node when necessary."""
+        self.add_node(node_id)
+        return self.IDmap[node_id]
+
+    def add_node(self, node_id):
+        """Create a new node.
+
+        Add a new node to the graph if not already exsitsed, by updating the
+        ID list, ID map, and the adjacency list data. Otherwise pass through
+        without further actions.
+
+        Note:
+            Does not raise error even if the node alrealy exists.
+
+        """
+        if node_id not in self.IDmap:
+            self.IDlst.append(node_id)
+            self.IDmap[node_id] = self.num_nodes
+            self._data.append({})
+
+    def add_edge(self, id1, id2, weight=1., directed=False):
+        """Add an edge to the graph.
+
+        Note:
+            Non-positive edges are ignored.
+
+        Args:
+            id1 (str): first node id.
+            id2 (str): second node id.
+            weight (float): the edge weight, default is 1.0
+            directed (bool): whether the edge is directed or not.
+
+        """
+        if self._is_valid_edge_weight(weight):
+            idx1, idx2 = map(self.get_node_idx, (id1, id2))
+            self._check_edge_existence(id1, id2, idx1, idx2, weight)
+
+            self._data[idx1][idx2] = weight
+            if not directed:
+                self._data[idx2][idx1] = weight
+
+    def read(self, edg_fp, weighted, directed):
         """Read an edgelist file and create sparse graph.
 
         Note:
             Implicitly discard zero weighted edges; if the same edge is defined
             multiple times with different edge weights, then the last specified
-            weight will be used (warning for such behavior will be printed)
+            weight will be used (warning for such behavior will be printed).
 
         Args:
             edg_fp (str): path to edgelist file, where the file is tab
@@ -77,55 +166,121 @@ class SparseGraph(IDHandle):
             directed (bool): whether the graph is directed, if undirected, the
                 edge connecting from destination node to source node is created
                 with same edge weight from source node to destination node.
-            csr (bool): whether or not to convert to compact sparse row format
-                after finished reading the whole edge list for a more compact
-                storage and more optimized cache utilization.
 
         """
-        current_node = 0
-
         with open(edg_fp, "r") as f:
+            for edge_line in f:
+                id1, id2, weight = self._read_edge_line(edge_line, weighted)
+                self.add_edge(id1, id2, weight, directed)
 
-            for line in f:
-                if weighted:
-                    id1, id2, weight = line.split("\t")
-                    weight = float(weight)
-                    if weight == 0:
-                        continue
-                else:
-                    terms = line.split("\t")
-                    id1, id2 = terms[0], terms[1]
-                    weight = float(1)
+    def to_csr(self):
+        """Construct compressed sparse row matrix."""
+        indptr = np.zeros(len(self.IDlst) + 1, dtype=np.uint32)
+        for i, row_data in enumerate(self._data):
+            indptr[i + 1] = indptr[i] + len(row_data)
 
-                id1 = id1.strip()
-                id2 = id2.strip()
+        # last element of indptr indicates the total number of nonzero entries
+        indices = np.zeros(indptr[-1], dtype=np.uint32)
+        data = np.zeros(indptr[-1], dtype=np.float64)
 
-                # check if ID exist, add to IDmap if not
-                for id_ in id1, id2:
-                    if id_ not in self.IDmap:
-                        self.IDmap[id_] = current_node
-                        self.data.append({})
-                        current_node += 1
+        for i in reversed(range(len(self._data))):
+            start = indptr[i]
+            end = indptr[i + 1]
 
-                idx1, idx2 = self.IDmap[id1], self.IDmap[id2]
-                # check if edge exists
-                if idx2 in self.data[idx1]:
-                    if self.data[idx1][idx2] != weight:
-                        print(
-                            f"WARNING: edge from {id1} to {id2} exists, with "
-                            f"value of {self.data[idx1][idx2]:.2f}. "
-                            f"Now overwrite to {weight:.2f}.",
-                        )
+            # TODO: clean up
+            tmp = self._data.pop()
+            sorted_keys = sorted(tmp)
 
-                # update edge weight
-                self.data[idx1][idx2] = weight
-                if not directed:
-                    self.data[idx2][idx1] = weight
+            indices[start:end] = np.fromiter(sorted_keys, dtype=np.uint32)
+            data[start:end] = np.fromiter(map(tmp.get, sorted_keys), dtype=np.float64)
 
-        self.IDlst = sorted(self.IDmap, key=self.IDmap.get)
+        return indptr, indices, data
 
-        if csr:
-            self.to_csr()
+    def from_mat(self, adj_mat, ids):
+        """Construct graph using adjacency matrix and node ids.
+
+        Args:
+            adj_mat(:obj:`numpy.ndarray`): 2D numpy array of adjacency matrix
+            ids(:obj:`list` of str): node ID list
+
+        """
+        data = []  # construct edge list
+        for row in adj_mat:
+            data.append({})
+            for j, weight in enumerate(row):
+                if weight != 0:
+                    data[-1][j] = weight
+
+        # save edgelist and id data and convert to csr format
+        self.data = data
+        self.set_ids(ids)
+
+    def to_dense(self):
+        """Construct dense adjacency matrix.
+
+        Note:
+            This method does not return DenseGraph object, but instead return
+            dense adjacency matrix as ``numpy.ndarray``, the index is the same
+            as that of IDlst.
+
+        Return:
+            numpy.ndarray: Full adjacency matrix as 2d numpy array.
+
+        """
+        n_nodes = len(self.IDlst)
+        mat = np.zeros((n_nodes, n_nodes))
+
+        for src_node, src_nbrs in enumerate(self._data):
+
+            for dst_node in src_nbrs:
+                mat[src_node, dst_node] = src_nbrs[dst_node]
+
+        return mat
+
+
+class SparseGraph(IDHandle):
+    """Sparse Graph object that stores graph as adjacency list.
+
+    Examples:
+        Read ``.edg`` file and create ``SparseGraph`` object using ``.read_edg``
+        method.
+
+        >>> from pecanpy.graph import SparseGraph
+        >>>
+        >>> # initialize SparseGraph object
+        >>> g = SparseGraph()
+        >>>
+        >>> # read graph from edgelist
+        >>> g.read_edg(path_to_edg_file, weighted=True, directed=False)
+        >>>
+        >>> # save the csr graph as npz file to be used later
+        >>> g.save(npz_outpath)
+
+    """
+
+    def __init__(self):
+        """Initialize SparseGraph object."""
+        super(SparseGraph, self).__init__()
+        self.data = []
+        self.indptr = None
+        self.indices = None
+
+    def read_edg(self, edg_fp, weighted, directed):
+        """Create CSR sparse graph from edge list.
+
+        First create ``AdjlstGraph`` by reading the edge list file, and then
+        convert to ``SparseGraph`` via ``to_csr``.
+
+        Args:
+            edg_fp (str): path to edgelist file.
+            weighted (bool): whether the graph is weighted.
+            directed (bool): whether the graph is directed.
+
+        """
+        g = AdjlstGraph()
+        g.read(edg_fp, weighted, directed)
+        self.set_ids(g.IDlst)
+        self.indptr, self.indices, self.data = g.to_csr()
 
     def read_npz(self, fp, weighted, directed):
         """Directly read a CSR sparse graph.
@@ -173,64 +328,10 @@ class SparseGraph(IDHandle):
             ids(:obj:`list` of str): node ID list
 
         """
-        data = []  # construct edge list
-        for row in adj_mat:
-            data.append({})
-            for j, weight in enumerate(row):
-                if weight != 0:
-                    data[-1][j] = weight
-
-        # save edgelist and id data and convert to csr format
-        self.data = data
-        self.set_ids(ids)
-        self.to_csr()
-
-    def to_csr(self):
-        """Construct compressed sparse row matrix."""
-        indptr = np.zeros(len(self.IDlst) + 1, dtype=np.uint32)
-        for i, row_data in enumerate(self.data):
-            indptr[i + 1] = indptr[i] + len(row_data)
-
-        # last element of indptr indicates the total number of nonzero entries
-        indices = np.zeros(indptr[-1], dtype=np.uint32)
-        data = np.zeros(indptr[-1], dtype=np.float64)
-
-        for i in reversed(range(len(self.data))):
-            start = indptr[i]
-            end = indptr[i + 1]
-
-            tmp = self.data.pop()
-            sorted_keys = sorted(tmp)
-
-            indices[start:end] = np.fromiter(sorted_keys, dtype=np.uint32)
-            data[start:end] = np.fromiter(map(tmp.get, sorted_keys), dtype=np.float64)
-
-        self.indptr = indptr
-        self.data = data
-        self.indices = indices
-
-    def to_dense(self):
-        """Construct dense adjacency matrix.
-
-        Note:
-            This method does not return DenseGraph object, but instead return
-            dense adjacency matrix as ``numpy.ndarray``, the index is the same
-            as that of IDlst.
-
-        Return:
-            numpy.ndarray: full adjacency matrix indexed by IDmap as 2d numpy
-            array.
-
-        """
-        n_nodes = len(self.IDlst)
-        mat = np.zeros((n_nodes, n_nodes))
-
-        for src_node, src_nbrs in enumerate(self.data):
-
-            for dst_node in src_nbrs:
-                mat[src_node, dst_node] = src_nbrs[dst_node]
-
-        return mat
+        g = AdjlstGraph()
+        g.from_mat(adj_mat, ids)
+        self.set_ids(g.IDlst)
+        self.indptr, self.indices, self.data = g.to_csr()
 
 
 class DenseGraph(IDHandle):
@@ -240,7 +341,9 @@ class DenseGraph(IDHandle):
         Read ``.npz`` files and create ``DenseGraph`` object using ``read_npz``.
 
         >>> from pecanpy.graph import DenseGraph
+        >>>
         >>> g = DenseGraph() # initialize DenseGraph object
+        >>>
         >>> g.read_npz(paht_to_npz_file, weighted=True, directed=False)
 
         Read ``.edg`` files and create ``DenseGraph`` object using ``read_edg``.
@@ -283,11 +386,11 @@ class DenseGraph(IDHandle):
 
     def read_edg(self, edg_fp, weighted, directed):
         """Read an edgelist file and construct dense graph."""
-        sparse_graph = SparseGraph()
-        sparse_graph.read_edg(edg_fp, weighted, directed, csr=False)
+        g = AdjlstGraph()
+        g.read(edg_fp, weighted, directed)
 
-        self.set_ids(sparse_graph.IDlst)
-        self.data = sparse_graph.to_dense()
+        self.set_ids(g.IDlst)
+        self.data = g.to_dense()
         self.nonzero = self.data != 0
 
     def from_mat(self, adj_mat, ids):
