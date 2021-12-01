5bd41ca82b29ac73fac3953a64c9924a7dee7573
Remy <liurenmi@msu.edu>
2021-11-30 19:33:28 -0500

    Update from_mat for SparseGraph and make from_adjlst_graph

--- src/pecanpy/graph.py
+++ src/pecanpy/graph.py
@@ -314,18 +314,49 @@ class SparseGraph(IDHandle):
             indices=self.indices,
         )
 
-    def from_mat(self, adj_mat, ids):
-        """Construct graph using adjacency matrix and node ids.
+    @classmethod
+    def from_adjlst_graph(cls, adjlst_graph):
+        """Construct csr graph from adjacency list graph.
+
+        Args:
+            adjlst_graph (:obj:`pecanpy.graph.AdjlstGraph`): Adjacency list
+                graph to be converted.
+
+        """
+        g = cls()
+        g.set_ids(adjlst_graph.IDlst)
+        g.indptr, g.indices, g.data = adjlst_graph.to_csr()
+        return g
+
+    @classmethod
+    def from_mat(cls, adj_mat, node_ids):
+        """Construct csr graph using adjacency matrix and node ids.
+
+        Note:
+            Only consider positive valued edges.
 
         Args:
             adj_mat(:obj:`numpy.ndarray`): 2D numpy array of adjacency matrix
-            ids(:obj:`list` of str): node ID list
+            node_ids(:obj:`list` of str): node ID list
 
         """
-        g = AdjlstGraph()
-        g.from_mat(adj_mat, ids)
-        self.set_ids(g.IDlst)
-        self.indptr, self.indices, self.data = g.to_csr()
+        g = cls()
+        g.set_ids(node_ids)
+
+        tot_num_edges = (adj_mat > 0).sum()
+        g.indptr = np.zeros(adj_mat.shape[0] + 1, dtype=np.uint32)
+        g.indices = np.zeros(tot_num_edges, dtype=np.uint32)
+        g.data = np.zeros(tot_num_edges, dtype=np.float64)
+
+        for i, row_data in enumerate(adj_mat):
+            nonzero_idx = np.where(row_data > 0)[0]
+            g.indptr[i + 1] = g.indptr[i] + nonzero_idx.size
+
+            chunk = slice(g.indptr[i], g.indptr[i + 1])
+            g.indices[chunk] = np.array(nonzero_idx, dtype=np.uint32)
+            g.data[chunk] = np.array(row_data[nonzero_idx], dtype=np.float64)
+
+        return g
 
 
 class DenseGraph(IDHandle):
