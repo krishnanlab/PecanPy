44ea1d561ab865845baf2c9e3c4cd2a46703e2ff
Remy <liurenmi@msu.edu>
2022-02-11 15:38:10 -0500

    Clean up PecanPy test

--- test/test_pecanpy.py
+++ test/test_pecanpy.py
@@ -2,6 +2,7 @@ import os.path as osp
 import unittest
 
 from numba import set_num_threads
+from parameterized import parameterized
 from pecanpy import graph
 from pecanpy import pecanpy
 
@@ -9,66 +10,35 @@ set_num_threads(1)
 
 DATA_DIR = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir, "demo"))
 EDG_FP = osp.join(DATA_DIR, "karate.edg")
+SETTINGS = [
+    ("SparseOTF", pecanpy.SparseOTF),
+    ("DenseOTF", pecanpy.DenseOTF),
+    ("PreComp", pecanpy.PreComp),
+    ("PreCompFirstOrder", pecanpy.PreCompFirstOrder),
+    ("FirstOrderUnweighted", pecanpy.FirstOrderUnweighted),
+]
 
 
-class TestPecanPyFromMat(unittest.TestCase):
-    def setUp(self):
+class TestPecanPy(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
         g = graph.DenseGraph()
         g.read_edg(EDG_FP, weighted=False, directed=False)
         self.mat = g.data
         self.ids = g.IDlst
-        self.kwargs = {"p": 1, "q": 1, "workers": 1}
 
-    def test_sparseotf_from_mat(self):
-        g = pecanpy.SparseOTF.from_mat(self.mat, self.ids, **self.kwargs)
-        g.embed()
+    @parameterized.expand(SETTINGS)
+    def test_from_mat(self, name, mode):
+        with self.subTest(name):
+            g = mode.from_mat(self.mat, self.ids, p=1, q=1)
+            g.embed()
 
-    def test_denseotf_from_mat(self):
-        g = pecanpy.DenseOTF.from_mat(self.mat, self.ids, **self.kwargs)
-        g.embed()
-
-    def test_precomp_from_mat(self):
-        g = pecanpy.PreComp.from_mat(self.mat, self.ids, **self.kwargs)
-        g.preprocess_transition_probs()
-        g.embed()
-
-    def test_precompfirtorder_from_mat(self):
-        g = pecanpy.PreCompFirstOrder.from_mat(self.mat, self.ids, **self.kwargs)
-        g.preprocess_transition_probs()
-        g.embed()
-
-    def test_firtorderunweighted_from_mat(self):
-        g = pecanpy.FirstOrderUnweighted.from_mat(self.mat, self.ids, **self.kwargs)
-        g.embed()
-
-
-class TestPecanPyFromEdg(unittest.TestCase):
-    def test_sparseotf_from_edg(self):
-        g = pecanpy.SparseOTF(1, 1, 1)
-        g.read_edg(EDG_FP, weighted=False, directed=False)
-        g.embed()
-
-    def test_denseotf_from_edg(self):
-        g = pecanpy.DenseOTF(1, 1, 1)
-        g.read_edg(EDG_FP, weighted=False, directed=False)
-        g.embed()
-
-    def test_precomp_from_edg(self):
-        g = pecanpy.PreComp(1, 1, 1)
-        g.read_edg(EDG_FP, weighted=False, directed=False)
-        g.preprocess_transition_probs()
-        g.embed()
-
-    def test_precompfirstorder_from_edg(self):
-        g = pecanpy.PreCompFirstOrder(1, 1, 1)
-        g.read_edg(EDG_FP, weighted=False, directed=False)
-        g.preprocess_transition_probs()
-        g.embed()
-
-    def test_firstorderunweighted_from_edg(self):
-        g = pecanpy.FirstOrderUnweighted(1, 1, 1)
-        g.read_edg(EDG_FP, weighted=False, directed=False)
-        g.embed()
+    @parameterized.expand(SETTINGS)
+    def test_from_edg(self, name, mode):
+        with self.subTest(name):
+            g = mode(p=1, q=1)
+            g.read_edg(EDG_FP, weighted=False, directed=False)
+            g.embed()
 
 
 if __name__ == "__main__":
