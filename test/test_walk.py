f562a9ba412ebd8dc981c3de478b60ceda6e6b27
Remy <liurenmi@msu.edu>
2022-02-11 14:30:26 -0500

    Add PreComp walk tests

--- test/test_walk.py
+++ test/test_walk.py
@@ -32,6 +32,30 @@ WALKS = {
         ["a", "b", "c", "d"],
         ["a", "b", "c", "b"],
     ],
+    "PreCompFirstOrder": [
+        ["c", "d", "e", "d"],
+        ["d", "c", "d", "e"],
+        ["e", "d", "c", "e"],
+        ["e", "d", "e", "c"],
+        ["b", "c", "e", "c"],
+        ["b", "c", "d", "c"],
+        ["c", "d", "e", "d"],
+        ["d", "c", "e", "d"],
+        ["a", "b", "a", "b"],
+        ["a", "b", "c", "e"],
+    ],
+    "PreComp": [
+        ["c", "d", "e", "d"],
+        ["d", "c", "d", "e"],
+        ["e", "d", "c", "e"],
+        ["e", "d", "e", "c"],
+        ["b", "c", "e", "c"],
+        ["b", "c", "d", "c"],
+        ["c", "d", "e", "d"],
+        ["d", "c", "e", "d"],
+        ["a", "b", "a", "b"],
+        ["a", "b", "c", "e"],
+    ],
     "SparseOTF": [
         ["c", "d", "e", "d"],
         ["d", "e", "c", "d"],
@@ -63,12 +87,15 @@ class TestWalk(unittest.TestCase):
     @parameterized.expand(
         [
             ("FirstOrderUnweighted", pecanpy.FirstOrderUnweighted),
+            ("PreCompFirstOrder", pecanpy.PreComp),
+            ("PreComp", pecanpy.PreComp),
             ("SparseOTF", pecanpy.SparseOTF),
             ("DenseOTF", pecanpy.DenseOTF),
         ],
     )
     def test_first_order_unweighted(self, name, mode):
         graph = mode.from_mat(MAT, IDS, p=1, q=1, random_state=0)
+        graph.preprocess_transition_probs()  # TODO: remove.
         walks = graph.simulate_walks(2, 3)
         self.assertEqual(walks, WALKS[name])
         print(walks)
