92b91e3acf1464b1620b2e2ba1a434ce8224039a
Remy <liurenmi@msu.edu>
2022-05-02 11:45:35 -0400

    Bump version to 2.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = "2020, Renming Liu and Arjun Krishnan"
 author = "Renming Liu and Arjun Krishnan"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.3"
+release = "2.0.5"
 
 
 # -- General configuration ---------------------------------------------------
