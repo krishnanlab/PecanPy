0d25b518e36a0266d5c1c7dd66aa7dab1ec1fdb6
RemyLau <remylau961@gmail.com>
2022-08-23 10:39:23 -0400

    bump version v2.0.7 -> 2.0.8-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = "2020, Renming Liu and Arjun Krishnan"
 author = "Renming Liu and Arjun Krishnan"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.7"
+release = "2.0.8-dev"
 
 
 # -- General configuration ---------------------------------------------------
