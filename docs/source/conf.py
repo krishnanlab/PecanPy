8a085e0c13bce10037a2bf273cc7840d707d35cb
RemyLau <remylau961@gmail.com>
2024-01-10 11:14:38 -0500

    bump version: 2.0.9 -> 2.0.10-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = "2020, Renming Liu and Arjun Krishnan"
 author = "Renming Liu and Arjun Krishnan"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.9"
+release = "2.0.10-dev"
 
 
 # -- General configuration ---------------------------------------------------
--- src/pecanpy/__init__.py
+++ src/pecanpy/__init__.py
@@ -2,5 +2,5 @@
 from . import graph
 from . import pecanpy
 
-__version__ = "2.0.9"
+version = "2.0.10-dev"
 __all__ = ["graph", "pecanpy"]
